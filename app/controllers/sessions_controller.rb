c93f388d0ae2e823099d78f4ced86916e19dc60d
Alex Butirskiy <butirskiy@gmail.com>
2015-08-24 07:24:01 +0300

    Some code from layouts moved to helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.password == params[:session][:password]
-      cookies[:user_status] = {value: 'signed_in', expires: 1.year.from_now}
-      cookies[:user_id] = {value: user.id, expires: 1.year.from_now}
+      cookies[:user_status] = { value: 'signed_in', expires: 1.year.from_now }
+      cookies[:user_id] = { value: user.id, expires: 1.year.from_now }
       redirect_to root_path
     else
-      render plain: "No such user"
+      render plain: 'No such user'
     end
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,7 @@
 module SessionsHelper
+
+  def signed?
+    @user_status == 'signed_in'
+  end
+  
 end
